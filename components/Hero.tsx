b5de8d1339b3f0a993b4a125e40e1cb2a4bb26e7
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2024-06-18 10:53:00 +0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,12 +52,12 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="just not easy, not impossible. enjoy your struggle!"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m rickthor7, a frontend Developer based in Indonesia.
           </p>
 
           <a href="#about">
